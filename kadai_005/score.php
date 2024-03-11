4deb0fc31f785ea4dfdf6d00100e32a011c8c875
maruko-chibimaru <136881382+maruko-chibimaru@users.noreply.github.com>
2024-03-11 10:28:24 +0900

    課題四則演算の再提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -22,9 +22,12 @@
         $score9 = 30;
         $score10 = 60;
 
-        echo $score1 + $score2 +  $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        $score_total = $score1 + $score2 +  $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        $score_average = $score_total /10;
+       
+        echo $score_total;
         echo '<br>';
-        echo ($score1 + $score2 +  $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+        echo $score_average;
        
     ?>
 
